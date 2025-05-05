ded5aa7a479f0af9d97b1dd688cca950fd947b30
MatteoCalvanico <matteo.calvanico03@gmail.com>
2025-05-05 16:18:29 +0200

    Add protected routes

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,22 +1,41 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
-// Import your pages
-import MqttPage from "./pages/MqttClient"; // Create this file with your current MQTT functionality
+import MqttPage from "./pages/MqttClient";
 import LoginPage from "./pages/LoginPage";
+import ProtectedRoute from "./components/ProtectedRoutes";
 
 function App() {
+  const isLoggedIn = localStorage.getItem("authToken") !== null;
+
   return (
     <BrowserRouter>
       <div className="App">
         <nav>
           <Link to="/client">MQTT Client</Link>
-          <Link to="/">Login</Link>
+          {isLoggedIn && (
+            <button 
+              onClick={() => {
+                localStorage.removeItem("authToken");
+                localStorage.removeItem("userId");
+                window.location.href = "/";
+              }}
+            >
+              Logout
+            </button>
+          )}
         </nav>
 
         <Routes>
-          <Route path="/client" element={<MqttPage />} />
           <Route path="/" element={<LoginPage />} />
+          <Route 
+            path="/client" 
+            element={
+              <ProtectedRoute>
+                <MqttPage />
+              </ProtectedRoute>
+            } 
+          />
         </Routes>
       </div>
     </BrowserRouter>
